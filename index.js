d08df2ac49669b1027459d911e8bec2b8f68aef2
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-04-23 21:18:43 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var example1 = require("./example1");
+var example2 = require("./example2");
+var example3 = require("./example3");
+
+example1.execute();
+
+example2.execute();
+
+example3.execute();
